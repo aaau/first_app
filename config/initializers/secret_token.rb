6cb807c623ec886ef9fec3f11af262b2d37a34e8
aaau <rikyo.takahashi@gmail.com>
2014-04-03 20:04:32 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'bdcb238b349377261731a15c11ff47c12b48ac586d6ddc2d7436fcbc52006a6271aa4cd03b5eb0b32605d6befe77dc2ef02a92768fe063400a92e0e9babe367b'
